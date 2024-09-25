1b9c46833bcc6a073a69559d3d7945827164e35b
Bithika Roy <roybithika188@gmail.com>
2024-09-25 15:07:13 +0530

    exams

--- program1.py
+++ program1.py
@@ -1,20 +1,38 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+import unittest
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        
+        for char in s:
+            if char in mapping:
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(char)
+        
+        return len(stack) == 0
 
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
 
+    def test_valid_parentheses(self):
+        self.assertTrue(self.solution.isValid("()"))
+        self.assertTrue(self.solution.isValid("()[]{}"))
+        self.assertTrue(self.solution.isValid("{[()]}"))
 
+    def test_invalid_parentheses(self):
+        self.assertFalse(self.solution.isValid("(]"))
+        self.assertFalse(self.solution.isValid("([)]"))
 
+    def test_empty_string(self):
+        self.assertTrue(self.solution.isValid(""))
 
+    def test_mixed_parentheses(self):
+        self.assertFalse(self.solution.isValid("(){"))
 
-    
-
-
-
-  
-
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
